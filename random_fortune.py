dd57ce17552ae27d69b184758e5353c0b6c73e10
Trevor Pahigian <68470054+tp-data@users.noreply.github.com>
2024-01-27 19:49:01 -0500

    add python file for random fortune

---
+++ random_fortune.py
@@ -0,0 +1,13 @@
+import random
+
+fortunes = [
+"Beware of low-flying ducks and unexpected banana peels today. \nYour ability to dodge both literal and metaphorical obstacles will be put to the test.",
+"Your coffee cup is trying to tell you something profound. \nUnfortunately, it's just too early for anyone to decipher the cryptic messages at the bottom.",
+"This month, you'll discover that the key to a happy life is not taking yourself too seriously. \nLaughter is the best medicine, but a good cup of coffee is a close second.",
+"Your pet goldfish has been plotting world domination from its bowl. \nKeep an eye on it, or you might find yourself taking orders from a fish with a Napoleon complex.",
+"Don't be surprised if your GPS develops a sarcastic attitude today. \nApparently, it's tired of being blamed for your questionable navigation skills."
+]
+
+print(random.choice(fortunes))
+
+print("\nAlso, you can merge 👍")
